6447489d091e1c22fa346350026378aa2a6d7133
Rafferty97 <alexander.drafferty@gmail.com>
2016-05-13 14:05:43 +0800

    Stripped out views and view engine

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send(JSON.stringify({
+    hello: 'world'
+  }));
 });
 
 module.exports = router;
